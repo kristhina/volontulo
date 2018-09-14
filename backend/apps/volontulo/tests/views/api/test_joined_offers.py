edb98a7711cd808b10714e52acffce594134600b
Krystyna <krystyna.m.piatkowska@gmail.com>
2018-09-14 12:37:24 +0200

    tests for api joined-offers, work in progress

--- backend/apps/volontulo/tests/views/api/test_joined_offers.py
+++ backend/apps/volontulo/tests/views/api/test_joined_offers.py
@@ -9,8 +9,7 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from apps.volontulo.factories import UserFactory, OrganizationFactory
-
-from backend.apps.volontulo.factories import OfferFactory
+from apps.volontulo.factories import OfferFactory
 
 ENDPOINT_URL = reverse('joined_offers')
 
@@ -18,25 +17,33 @@ ENDPOINT_URL = reverse('joined_offers')
 class TestJoinedOffers(APITestCase, TestCase):
 
     def test_user_joined_no_offers(self):
+
         user = UserFactory()
-        res = self.client.get(ENDPOINT_URL)
-        self.assertEqual(res, [])
+        self.client.login(username=user.username, password=user.password)
+
+        res = self.client.get(ENDPOINT_URL, {'username': user.username, 'password': user.password}, format = 'json')
+        self.assertEqual(res.data, [])
 
     def test_user_joined_one_offer(self):
+        # import pdb;
+        # pdb.set_trace()
         user = UserFactory()
+        self.client.login(username=user.username, password=user.password)
         offer = OfferFactory(
             volunteers=[user]
         )
-        res = self.client.get(ENDPOINT_URL)
-        self.assertEqual(len(res), 1)
+        res = self.client.get(ENDPOINT_URL, {'username': user.username, 'password': user.password}, format = 'json')
+        self.assertEqual(len(res.data), 1)
 
     def test_user_joined_some_offers(self):
         user = UserFactory()
+        self.client.login(username=user.username, password=user.password)
+
         offer1 = OfferFactory(
             volunteers=[user]
         )
         offer2 = OfferFactory(
             volunteers=[user]
         )
-        res = self.client.get(ENDPOINT_URL)
-        self.assertEqual(len(res), 2)
+        res = self.client.get(ENDPOINT_URL, {'username': user.username, 'password': user.password}, format = 'json')
+        self.assertEqual(len(res.data), 2)
