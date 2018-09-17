86be54bad2d088624dc689aba7d0d59a11cedef7
Krystyna <krystyna.m.piatkowska@gmail.com>
2018-09-17 14:16:31 +0200

    tests working for status code and list lenght

--- backend/apps/volontulo/tests/views/api/test_joined_offers.py
+++ backend/apps/volontulo/tests/views/api/test_joined_offers.py
@@ -15,8 +15,10 @@ ENDPOINT_URL = reverse('joined_offers')
 
 
 class TestJoinedOffers(APITestCase, TestCase):
+    """ Tests for joined-offers api"""
 
     def test_user_joined_no_offers(self):
+        """ Tests if user did not join any offer"""
 
         user = UserFactory()
         self.client.force_login(user=user)
@@ -26,26 +28,30 @@ class TestJoinedOffers(APITestCase, TestCase):
         self.assertEqual(res.data, [])
 
     def test_user_joined_one_offer(self):
+        """Tests if user joined one offer"""
 
         user = UserFactory()
         self.client.force_login(user=user)
 
-        offer = OfferFactory()
+        offer = OfferFactory(image=None)
         offer.volunteers.add(user)
 
         res = self.client.get(ENDPOINT_URL, {'username': user.username, 'password': user.password}, format='json')
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(len(res.data), 1)
 
     def test_user_joined_some_offers(self):
-        user = UserFactory()
+        """Tests if user joined more than one offer"""
 
+        user = UserFactory()
         self.client.force_login(user=user)
 
-        offer1 = OfferFactory(
-            volunteers=[user]
-        )
-        offer2 = OfferFactory(
-            volunteers=[user]
-        )
+        offer1 = OfferFactory(image=None)
+        offer1.volunteers.add(user)
+        offer2 = OfferFactory(image=None)
+        offer2.volunteers.add(user)
+        offer3 = OfferFactory(image=None)
+
         res = self.client.get(ENDPOINT_URL, {'username': user.username, 'password': user.password}, format='json')
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(len(res.data), 2)
